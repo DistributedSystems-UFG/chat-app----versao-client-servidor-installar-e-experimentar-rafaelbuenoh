a7c2e8d7bea7cfa520d14a942a7bd0d593ca9147
rafaelbuenoh <61986764+rafaelbuenoh@users.noreply.github.com>
2022-09-14 14:25:46 -0300

    Update const.py

--- const.py
+++ const.py
@@ -1,5 +1,5 @@
-CHAT_SERVER_HOST = "10.128.0.3"
+CHAT_SERVER_HOST = "ec2-34-203-252-10.compute-1.amazonaws.com"
 CHAT_SERVER_PORT = 5001
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
-registry = {"Alice":("10.128.0.2",5002), "Bob":("10.128.0.4",5002)}
+registry = {"Alice":("ec2-44-203-41-123.compute-1.amazonaws.com",5002), "Bob":("ec2-3-83-120-228.compute-1.amazonaws.com",5002), "Charlie":("ec2-54-86-8-95.compute-1.amazonaws.com", 5002)}
